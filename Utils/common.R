14409185717e2beb6830845ec383ccbaf5cf71ee
Alex <rates.quant@gmail.com>
2017-08-11 12:55:25 -0400

    added error checking in interaction score calculations

--- Utils/common.R
+++ Utils/common.R
@@ -50,10 +50,12 @@ cor.mtest <- function(mat, conf.level = 0.95){
 gbm_interactions <- function(gbm_model, data, iter, min_influence = 1, degree = 2){
   gbm_summary = summary(gbm_model, plotit=FALSE)
   vars = gbm_summary$var[gbm_summary$rel.inf > min_influence]
-  all_combinations = combn(as.vector(vars), degree, simplify = TRUE)
+  all_combinations = combn(as.character(vars), degree, simplify = TRUE)
   df = ldply(seq(dim(all_combinations)[2]), function(i) {
-    data.frame(vars = paste(all_combinations[,i], collapse = '|'), 
-               interaction_score = interact.gbm(gbm_model, data, all_combinations[,i], n.trees = iter)) 
+    interaction_score = try( interact.gbm(gbm_model, data, all_combinations[,i], n.trees = iter))
+    if(!is.numeric(interaction_score)) 
+      interaction_score = NA
+    data.frame(vars = paste(all_combinations[,i], collapse = '|'), interaction_score = interaction_score)
   })
   return ( df[order(df$interaction_score, decreasing = TRUE),] )
 }
